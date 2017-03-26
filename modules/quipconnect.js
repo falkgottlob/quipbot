c322410b0f6e04dd96210c344752d9fdc2690154
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-25 17:40:30 -0700

    update

--- modules/quipconnect.js
+++ modules/quipconnect.js
@@ -1,44 +1,107 @@
 "use strict";
 
-let request = require('request'),
-    FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+let quip = require('./modules/quip.js'),
 
-exports.send = (message, recipient) => {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: FB_PAGE_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipient},
-            message: message
-        }
-    }, (error, response) => {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
+    QUIP_TOKEN = process.env.QUIP_TOKEN,
+
+    qclient = new quip.Client({accessToken: QUIP_TOKEN}),
+    rclient = new require('node-rest-client').Client,
+    client = new require('websocket').client;
+
+
+var args = {
+    headers: { "Authorization": "Bearer " + process.env.QUIP_TOKEN  } // request headers 
 };
+var url;
+ 
+// direct way 
+rclient.get("https://platform.quip.com/1/websockets/new", args, function (data, response) {
+    // parsed response body as js object 
+    url = data;
+    client.on('connectFailed', function(error) {
+        console.log('QUIP Connect Error: ' + error.toString());
+    });
 
-exports.getUserInfo = (userId) => {
-
-    return new Promise((resolve, reject) => {
-
-        request({
-            url: `https://graph.facebook.com/v2.6/${userId}`,
-            qs: {fields:"first_name,last_name,profile_pic", access_token: FB_PAGE_TOKEN},
-            method: 'GET',
-        }, (error, response) => {
-            if (error) {
-                console.log('Error sending message: ', error);
-                reject(error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            } else {
-                resolve(JSON.parse(response.body));
+    client.on('connect', function(connection) {
+        console.log("QUIP WebSocket client connected");
+        connection.on('error', function(error) {
+            console.log("QUIP Connection Error: " + error.toString());
+        });
+        connection.on('close', function() {
+            console.log("QUIP echo-protocol Connection Closed");
+            setTimeout(startnew, 5000);
+        })
+        connection.on('message', function(message) {
+            if (message.type === 'utf8') {
+                console.log("Received: '" + message.utf8Data + "'");
+                parseMessage(message);
             }
         });
+        
+        function parseMessage(message) {
+            if (connection.connected) {
+                var mess = JSON.parse(message.utf8Data);
+                if(mess.type == 'message'){
 
-    });
-};
+
+
+
+    let events = req.body.entry[0].messaging;
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i];
+        let sender = event.sender.id;
+        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+        } else if (event.message && event.message.text) {
+            let result = processor.match(event.message.text);
+            if (result) {
+                let handler = handlers[result.handler];
+                if (handler && typeof handler === "function") {
+                    handler(sender, result.match);
+                } else {
+                    console.log("Handler " + result.handlerName + " is not defined");
+                }
+            }
+        } else if (event.postback) {
+            let payload = event.postback.payload.split(",");
+            let postback = postbacks[payload[0]];
+            if (postback && typeof postback === "function") {
+                postback(sender, payload);
+            } else {
+                console.log("Postback " + postback + " is not defined");
+            }
+        } else if (event.message && event.message.attachments) {
+            uploads.processUpload(sender, event.message.attachments);
+        }
+    }
+    res.sendStatus(200);
+
+
+
+
+
+
+
+
+
+
+
+                } 
+            }
+            
+        }
+        function sendNumber() {
+            if (connection.connected) {
+                var number = Math.round(Math.random() * 0xFFFFFF);
+                connection.sendUTF(number.toString());
+                setTimeout(sendNumber, 1000);
+            }
+        }
+       
+
+        sendNumber();
+         //quip expects origin to be set. 
+        client.connect(url.url, null, 'https://quip.com' , null , null);
+});
+
+
