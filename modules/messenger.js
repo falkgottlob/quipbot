46284047953c377e5c60a53258e892b7c3cb34ea
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-25 19:15:22 -0700

    dd

--- modules/messenger.js
+++ modules/messenger.js
@@ -36,7 +36,6 @@ rclient.get("https://platform.quip.com/1/websockets/new", args, function (data, 
         });
         connection.on('message', function(message) {
             if (message.type === 'utf8') {
-                // && message.type == 'message'
                 parseMessage(message);
             }
         });
@@ -50,38 +49,40 @@ function parseMessage(message) {
      
     let events = JSON.parse(message.utf8Data);
     console.log("QUIP parsing Type: " + events.type); 
-    console.log("QUIP parsing Text: " + events.message.text); 
+    
+    if(events.type == 'message'){
+        console.log("QUIP parsing Text: " + events.message.text); 
 
-    //let events = req.body.entry[0].messaging;
-    for (let i = 0; i < events.length; i++) {
-        let event = events[i];
-        let sender = event.sender.id;
-        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
-        } else if (event.message && event.message.text) {
-            let result = processor.match(event.message.text);
-            if (result) {
-                let handler = handlers[result.handler];
-                if (handler && typeof handler === "function") {
-                    handler(sender, result.match);
-                } else {
-                    console.log("Handler " + result.handlerName + " is not defined");
+        //let events = req.body.entry[0].messaging;
+        for (let i = 0; i < events.length; i++) {
+            let event = events[i];
+            let sender = event.sender.id;
+            if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+                sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+            } else if (event.message && event.message.text) {
+                let result = processor.match(event.message.text);
+                if (result) {
+                    let handler = handlers[result.handler];
+                    if (handler && typeof handler === "function") {
+                        handler(sender, result.match);
+                    } else {
+                        console.log("Handler " + result.handlerName + " is not defined");
+                    }
                 }
+            } else if (event.postback) {
+                let payload = event.postback.payload.split(",");
+                let postback = postbacks[payload[0]];
+                if (postback && typeof postback === "function") {
+                    postback(sender, payload);
+                } else {
+                    console.log("Postback " + postback + " is not defined");
+                }
+            } else if (event.message && event.message.attachments) {
+                uploads.processUpload(sender, event.message.attachments);
             }
-        } else if (event.postback) {
-            let payload = event.postback.payload.split(",");
-            let postback = postbacks[payload[0]];
-            if (postback && typeof postback === "function") {
-                postback(sender, payload);
-            } else {
-                console.log("Postback " + postback + " is not defined");
-            }
-        } else if (event.message && event.message.attachments) {
-            uploads.processUpload(sender, event.message.attachments);
         }
+        //res.sendStatus(200);
     }
-    //res.sendStatus(200);
-
 
 }
        
