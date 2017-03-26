3d6bae451a624f03f6bc5e27aea43275e985a8a8
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-25 17:56:58 -0700

    cc

--- modules/messenger.js
+++ modules/messenger.js
@@ -46,35 +46,35 @@ rclient.get("https://platform.quip.com/1/websockets/new", args, function (data, 
 
 
 
-    let events = req.body.entry[0].messaging;
-    for (let i = 0; i < events.length; i++) {
-        let event = events[i];
-        let sender = event.sender.id;
-        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
-        } else if (event.message && event.message.text) {
-            let result = processor.match(event.message.text);
-            if (result) {
-                let handler = handlers[result.handler];
-                if (handler && typeof handler === "function") {
-                    handler(sender, result.match);
-                } else {
-                    console.log("Handler " + result.handlerName + " is not defined");
-                }
-            }
-        } else if (event.postback) {
-            let payload = event.postback.payload.split(",");
-            let postback = postbacks[payload[0]];
-            if (postback && typeof postback === "function") {
-                postback(sender, payload);
-            } else {
-                console.log("Postback " + postback + " is not defined");
-            }
-        } else if (event.message && event.message.attachments) {
-            uploads.processUpload(sender, event.message.attachments);
-        }
-    }
-    res.sendStatus(200);
+                    let events = req.body.entry[0].messaging;
+                    for (let i = 0; i < events.length; i++) {
+                        let event = events[i];
+                        let sender = event.sender.id;
+                        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+                            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+                        } else if (event.message && event.message.text) {
+                            let result = processor.match(event.message.text);
+                            if (result) {
+                                let handler = handlers[result.handler];
+                                if (handler && typeof handler === "function") {
+                                    handler(sender, result.match);
+                                } else {
+                                    console.log("Handler " + result.handlerName + " is not defined");
+                                }
+                            }
+                        } else if (event.postback) {
+                            let payload = event.postback.payload.split(",");
+                            let postback = postbacks[payload[0]];
+                            if (postback && typeof postback === "function") {
+                                postback(sender, payload);
+                            } else {
+                                console.log("Postback " + postback + " is not defined");
+                            }
+                        } else if (event.message && event.message.attachments) {
+                            uploads.processUpload(sender, event.message.attachments);
+                        }
+                    }
+                    res.sendStatus(200);
 
 
 
@@ -102,6 +102,7 @@ rclient.get("https://platform.quip.com/1/websockets/new", args, function (data, 
         sendNumber();
          //quip expects origin to be set. 
         client.connect(url.url, null, 'https://quip.com' , null , null);
+};
 });
 
 
