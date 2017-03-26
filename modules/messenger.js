74f6dd87f9ff9f903d073f2f3c91d66f20bc4c11
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-25 19:28:39 -0700

    message

--- modules/messenger.js
+++ modules/messenger.js
@@ -1,19 +1,16 @@
 "use strict";
 
 let quip = require('./quip.js'),
-
-    QUIP_TOKEN = process.env.QUIP_TOKEN;
-
-var Client = require('node-rest-client').Client;
-var rclient = new Client();
-
-var WebSocketClient = require('websocket').client;
-var client = new WebSocketClient();
-
-var args = {
-    headers: { "Authorization": "Bearer " + QUIP_TOKEN  } // request headers 
-};
-var url;
+    QUIP_TOKEN = process.env.QUIP_TOKEN,
+    Client = require('node-rest-client').Client,
+    rclient = new Client(),
+    WebSocketClient = require('websocket').client,
+    client = new WebSocketClient(),
+    qclient = new quip.Client({accessToken: process.env.QUIP_TOKEN}),
+    args = {
+        headers: { "Authorization": "Bearer " + QUIP_TOKEN  } // request headers 
+    },
+    url;
  
 // direct way 
 rclient.get("https://platform.quip.com/1/websockets/new", args, function (data, response) {
@@ -53,13 +50,28 @@ function parseMessage(message) {
     if(events.type == 'message'){
         console.log("QUIP parsing Text: " + events.message.text); 
 
+            let thread = events.thread.id;
+            let annotation = '';
+            if(events.message.annotation){
+              annotation = events.message.annotation.id;
+            }
+            var records = [];
+
+            sendMessage(thread, 'error', annotation, null);
+
         //let events = req.body.entry[0].messaging;
         for (let i = 0; i < events.length; i++) {
             let event = events[i];
             let sender = event.sender.id;
+
+
             if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-                sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+                //sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+
+
             } else if (event.message && event.message.text) {
+
+
                 let result = processor.match(event.message.text);
                 if (result) {
                     let handler = handlers[result.handler];
@@ -70,6 +82,8 @@ function parseMessage(message) {
                     }
                 }
             } else if (event.postback) {
+
+
                 let payload = event.postback.payload.split(",");
                 let postback = postbacks[payload[0]];
                 if (postback && typeof postback === "function") {
@@ -84,5 +98,13 @@ function parseMessage(message) {
         //res.sendStatus(200);
     }
 
+}
+
+function sendMessage(thread, message, responseto, parts){
+
+   //parts need very specific generation, too much effort so took it out
+    var newmessages = { threadId : thread , content : message, annotation_id : responseto};
+   
+    qclient.newMessage(newmessages, sentmessage);
 }
        
