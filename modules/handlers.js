2a2e1fef3747aa0b8dd2223bf630e29962f01f25
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-26 16:55:41 -0700

    d

--- modules/handlers.js
+++ modules/handlers.js
@@ -13,49 +13,49 @@ exports.contact = (thread, values) => {
 };
 
 exports.searchHouse = (thread) => {
-    messenger.addSection({text: `OK, looking for houses for sale around you...}`}, thread);
+    messenger.addSection('OK, looking for houses for sale around you...', thread);
     salesforce.findProperties().then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.searchHouse_City = (thread, values) => {
-    messenger.addSection({text: `OK, looking for houses in ${values[1]}`}, thread);
+    messenger.addSection('OK, looking for houses in ${values[1]}', thread);
     salesforce.findProperties({city: values[1]}).then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.searchHouse_Bedrooms_City_Range = (thread, values) => {
-    messenger.addSection({text: `OK, looking for ${values[1]} bedrooms in ${values[2]} between ${values[3]} and ${values[4]}`}, thread);
+    messenger.addSection('OK, looking for ${values[1]} bedrooms in ${values[2]} between ${values[3]} and ${values[4]}', thread);
     salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.searchHouse_Bedrooms_City = (thread, values) => {
-    messenger.addSection({text: `OK, looking for ${values[1]} bedroom houses in ${values[2]}`}, thread);
+    messenger.addSection('OK, looking for ${values[1]} bedroom houses in ${values[2]}', thread);
     salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.searchHouse_Bedrooms = (thread, values) => {
-    messenger.addSection({text: `OK, looking for ${values[1]} bedrooms`}, thread);
+    messenger.addSection('OK, looking for ${values[1]} bedrooms', thread);
     salesforce.findProperties({bedrooms: values[1]}).then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.searchHouse_Range = (thread, values) => {
-    messenger.addSection({text: `OK, looking for houses between ${values[1]} and ${values[2]}`}, thread);
+    messenger.addSection('OK, looking for houses between ${values[1]} and ${values[2]}', thread);
     salesforce.findProperties({priceMin: values[1], priceMax: values[2]}).then(properties => {
         messenger.addSection(formatter.formatProperties(properties), thread);
     });
 };
 
 exports.priceChanges = (thread, values) => {
-    messenger.addSection({text: `OK, looking for recent price changes...`}, thread);
+    messenger.addSection('OK, looking for recent price changes...', thread);
     salesforce.findPriceChanges().then(priceChanges => {
         messenger.addSection(formatter.formatPriceChanges(priceChanges), thread);
     });
@@ -68,6 +68,6 @@ exports.hi = (thread) => {
 };
 
 exports.help = (thread) => {
-    messenger.sendMessage({text: `You can ask me questions like "Find houses in Boston", "3 bedrooms in Boston", 
-        "3 bedrooms in Boston between 500000 and 750000", "show me price changes"`}, thread, "", null);
+    messenger.sendMessage('You can ask me questions like "Find houses in Boston", "3 bedrooms in Boston", 
+        "3 bedrooms in Boston between 500000 and 750000", "show me price changes"', thread, "", null);
 };
