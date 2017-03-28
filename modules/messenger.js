ae017b35b4cca817ba1e679a77a0065aba07b66c
Falk Gottlob <fgottlob@fgottlob-ltm2.internal.salesforce.com>
2017-03-28 14:53:35 -0700

    dd

--- modules/messenger.js
+++ modules/messenger.js
@@ -128,11 +128,10 @@ let getQuipContent = (quipid, thread) => {
     //console.log("QUIP sentedit: " + sentedit); 
     qclient.getThread(quipid, function(err, threads){
         if (err) {  console.error(err); }
-        onsole.log("@@@@@@@@@@QUIP : " + threads.html); 
+        console.log("@@@@@@@@@@QUIP : " + threads.html); 
         html = cheerio.load(threads.html);
    
     });
-    c
     console.log(html);
     return html;   
     
